df2f8508a4c100e2567be8257127447572114caf
Busra <girzap92@gmail.com>
2020-06-28 16:58:36 -0500

    thirrd commit from local repo

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,8 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
-		System.out.println("Second commut from local repository");
-
+		System.out.println("Second commit from local repository");
+        System.out.println("Third commit from local repository");
 	}
 
 }
