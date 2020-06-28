9aaab2a4724c6a2359568db85bdbba04b7800346
Busra <girzap92@gmail.com>
2020-06-28 16:53:31 -0500

    Second commit from local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
+		System.out.println("Second commut from local repository");
 
 	}
 
